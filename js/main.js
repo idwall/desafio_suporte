44ce58f7f2a325bb5d4ee0718495823b0228d238
Guilherme Cesar Leite <guilhermecesarleite@gmail.com>
2018-05-17 18:22:26 -0300

    final v1

--- js/main.js
+++ js/main.js
@@ -1,62 +1,84 @@
 
-(function ($) {
+(($) => {
     "use strict";
 
-    
+
     /*==================================================================
     [ Validate ]*/
-    var name = $('.validate-input input[name="name"]');
-    var email = $('.validate-input input[name="email"]');
-    var subject = $('.validate-input input[name="subject"]');
-    var message = $('.validate-input textarea[name="message"]');
+    const name = $('.validate-input input[name="name"]');
+    const email = $('.validate-input input[name="mail"]');
+    const subject = $('.validate-input input[name="subject"]');
+    const message = $('.validate-input textarea[name="message"]');
 
+    const showValidate = (input) => {
+        const thisAlert = $(input).parent();
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+        $(thisAlert).addClass('alert-validate');
+    };
 
-        if($(name).val().trim() == ''){
+    const hideValidate = (input) => {
+        const thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+    $('.validate-form').on('submit', (e) => {
+        let check = true;
+
+        if ($(name) && $(name).val() && $(name).val().trim() == '') {
             showValidate(name);
-            check=false;
+            check = false;
         }
 
-        if($(subject).val().trim() == ''){
+        if ($(subject) && $(subject).val() && $(subject).val().trim() == '') {
             showValidate(subject);
-            check=false;
+            check = false;
         }
 
-
-        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+        if ($(email) && $(email).val() &&
+            $(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
             showValidate(email);
-            check=false;
+            check = false;
         }
 
-        if($(message).val().trim() == ''){
+        if ($(message) && $(message).val() && $(message).val().trim() == '') {
             showValidate(message);
-            check=false;
+            check = false;
+        }
+
+        e.preventDefault();
+
+        if (check) {
+            const url = 'https://webhook.site/042d5c5d-2d54-4646-a26f-7a103ca71163';
+
+            const data = {
+                name: $(name).val(),
+                email: $(email).val(),
+                subject: $(subject).val(),
+                message: $(message).val()
+            };
+
+            const responseCallback = () => {
+                alert('Obrigado pela mensagem! Entraremos em contato via E-mail.');
+            }
+
+            $.ajax({
+                type: 'POST',
+                url,
+                crossDomain: true,
+                data: JSON.stringify(data),
+                complete: responseCallback,
+            });
         }
 
         return check;
     });
 
 
-    $('.validate-form .input1').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-       });
+    $('.validate-form .input1').each(() => {
+        $(this).focus(() => {
+            hideValidate(this);
+        });
     });
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
 
 })(jQuery);
