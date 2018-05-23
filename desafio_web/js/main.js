9cbd80fb6b9ca7da01cbabdf8219333db4cd337d
gustavo <gustavolimatti@outlook.com>
2018-05-23 18:13:15 -0300

    fixing validation

--- desafio_web/js/main.js
+++ desafio_web/js/main.js
@@ -25,31 +25,38 @@
     $('.validate-form').on('submit', (e) => {
         let check = true;
 
-        if ($(name) && $(name).val() && $(name).val().trim() == '') {
-            showValidate(name);
+        if ($('.validate-input input[name="name"]').val().trim() == '') {
+            showValidate($('.validate-input input[name="name"]'));
             check = false;
+        } else {
+            hideValidate($('.validate-input input[name="name"]'))
         }
 
-        if ($(subject) && $(subject).val() && $(subject).val().trim() == '') {
-            showValidate(subject);
+        if ($('.validate-input input[name="email"]').val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate($('.validate-input input[name="email"]'));
             check = false;
+        } else {
+            hideValidate($('.validate-input input[name="email"]'))
         }
 
-        if ($(email) && $(email).val() &&
-            $(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            showValidate(email);
+        if ($('.validate-input input[name="subject"]').val().trim() == '') {
+            showValidate($('.validate-input input[name="subject"]'));
             check = false;
+        } else {
+            hideValidate($('.validate-input input[name="subject"]'))
         }
-
-        if ($(message) && $(message).val() && $(message).val().trim() == '') {
-            showValidate(message);
+        
+        if ($('.validate-input textarea[name="message"]').val().trim() == '') {
+            showValidate($('.validate-input textarea[name="message"]'));
             check = false;
+        } else {
+            hideValidate($('.validate-input textarea[name="message"]'))
         }
 
         e.preventDefault();
 
         if (check) {
-            const url = 'https://webhook.site/042d5c5d-2d54-4646-a26f-7a103ca71163';
+            const url = 'https://webhook.site/d9288eaf-9f5a-4df6-8ace-44fd06e66b12';
 
             const data = {
                 name: $(name).val(),
